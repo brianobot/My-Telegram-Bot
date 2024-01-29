cb87ef94ee041aade4b471f13735b45e0d7b1dce
Brian Obot <brianobot9@gmail.com>
2024-01-29 14:38:16 +0100

    Install Black for linting and apply black formatter to the codebase

--- bot.py
+++ bot.py
@@ -8,12 +8,15 @@ API_TOKEN = env("API_TOKEN")
 
 bot = telebot.TeleBot(API_TOKEN)
 
-@bot.message_handler(commands=['start', 'hello'])
+
+@bot.message_handler(commands=["start", "hello"])
 def send_welcome(message):
     bot.reply_to(message, "Howdy, how are you doing?")
+
 
 @bot.message_handler(func=lambda msg: True)
 def echo_all(message):
     bot.reply_to(message, message.text)
+
 
 bot.infinity_polling()
