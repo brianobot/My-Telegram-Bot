d9bca7a80eea32ee27ece11a6d0e6bff413db977
Brian Obot <brianobot9@gmail.com>
2024-01-29 14:37:07 +0100

    Update the initial working version of the bot 🙂

---
+++ bot.py
@@ -0,0 +1,19 @@
+import environ
+import telebot
+
+env = environ.Env()
+environ.Env.read_env()
+
+API_TOKEN = env("API_TOKEN")
+
+bot = telebot.TeleBot(API_TOKEN)
+
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(func=lambda msg: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+bot.infinity_polling()
